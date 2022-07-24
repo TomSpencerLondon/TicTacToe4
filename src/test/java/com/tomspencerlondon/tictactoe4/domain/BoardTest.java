feb2d3ac232bcaec2ccc03268db59073ffed2b66
Tom <thomas.spencer@codurance.com>
2022-07-24 19:57:33 +0100

    Refactor: Add test for more than one piece

--- src/test/java/com/tomspencerlondon/tictactoe4/domain/BoardTest.java
+++ src/test/java/com/tomspencerlondon/tictactoe4/domain/BoardTest.java
@@ -22,5 +22,22 @@ class BoardTest {
 
     assertThat(board.asString())
         .isEqualTo("X________");
+    assertThat(board.contains(0, "X"))
+        .isTrue();
+  }
+
+  @Test
+  void givenTwoPlaysBoardContainsPositions() {
+    Board board = new Board();
+
+    board.play(0, "X");
+    board.play(1, "O");
+
+    assertThat(board.asString())
+        .isEqualTo("XO_______");
+    assertThat(board.contains(0, "X"))
+        .isTrue();
+    assertThat(board.contains(1, "O"))
+        .isTrue();
   }
 }
