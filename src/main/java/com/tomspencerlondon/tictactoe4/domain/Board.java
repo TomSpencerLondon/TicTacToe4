a29f1e025e4cb1808aa155fceb765a049a4baca6
Tom <thomas.spencer@codurance.com>
2022-07-24 20:28:15 +0100

    Refactor

--- src/main/java/com/tomspencerlondon/tictactoe4/domain/Board.java
+++ src/main/java/com/tomspencerlondon/tictactoe4/domain/Board.java
@@ -1,6 +1,7 @@
 package com.tomspencerlondon.tictactoe4.domain;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Board {
@@ -34,24 +35,7 @@ public class Board {
   }
 
   private Place position(int position) {
-    if (position == 0) {
-      return new Place(0, 0);
-    } else if (position == 1) {
-      return new Place(0, 1);
-    } else if (position == 2) {
-      return new Place(0, 2);
-    } else if (position == 3) {
-      return new Place(1, 0);
-    } else if (position == 4) {
-      return new Place(1, 1);
-    } else if (position == 5) {
-      return new Place(1, 2);
-    } else if (position == 6) {
-      return new Place(2, 0);
-    } else if (position == 7) {
-      return new Place(2, 1);
-    } else {
-      return new Place(2, 2);
-    }
+    List<Place> places = List.of(new Place(0, 0), new Place(0, 1), new Place(0, 2), new Place(1, 0), new Place(1, 1), new Place(1, 2), new Place(2, 0), new Place(2, 1), new Place(2, 2));
+    return places.get(position);
   }
 }
