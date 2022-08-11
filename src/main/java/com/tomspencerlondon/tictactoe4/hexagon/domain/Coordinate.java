dccaeb2f23950ecaec411fdb5e39a5adcac59024
Tom Spencer <thomas.spencer@codurance.com>
2022-08-11 12:58:24 +0100

    Add Coordinate calculation to fromMove

--- src/main/java/com/tomspencerlondon/tictactoe4/hexagon/domain/Coordinate.java
+++ src/main/java/com/tomspencerlondon/tictactoe4/hexagon/domain/Coordinate.java
@@ -1,11 +1,8 @@
 package com.tomspencerlondon.tictactoe4.hexagon.domain;
 
-import java.util.List;
-
 public record Coordinate(int x, int y) {
 
   public static Coordinate fromMove(int move) {
-    return List.of(new Coordinate(0, 0), new Coordinate(0, 1), new Coordinate(0, 2), new Coordinate(1, 0), new Coordinate(1, 1), new Coordinate(1, 2), new Coordinate(2, 0), new Coordinate(2, 1), new Coordinate(2, 2))
-        .get(move);
+    return new Coordinate(move / 3, move % 3);
   }
 }
