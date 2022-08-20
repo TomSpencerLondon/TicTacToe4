3043257d154fdc6a5c97db179c14ba0c6032d9a4
Tom Spencer <thomas.spencer@codurance.com>
2022-08-20 19:49:12 +0100

    F: WebSocketConfiguration

--- src/main/java/com/tomspencerlondon/tictactoe4/WebSocketConfig.java
+++ src/main/java/com/tomspencerlondon/tictactoe4/WebSocketConfig.java
@@ -1,23 +1,23 @@
 package com.tomspencerlondon.tictactoe4;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/mywebsockets")
-        .withSockJS();
+    registry.addEndpoint("/websocket");
   }
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/topic", "queue");
+    config.enableSimpleBroker("/tictactoe");
     config.setApplicationDestinationPrefixes("/app");
   }
-
 }
